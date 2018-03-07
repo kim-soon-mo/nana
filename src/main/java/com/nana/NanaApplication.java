a5f0323f6c46fd29bb51ff184d508ac0bb4da00c
wmp-2013112501 <rlatnsah@wemakeprice.com>
2018-03-07 16:05:10 +0900

    #fix-project start

---
+++ src/main/java/com/nana/NanaApplication.java
@@ -0,0 +1,12 @@
+package com.nana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanaApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nana/web/controllers/NanaController.java
@@ -0,0 +1,16 @@
+package com.nana.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wmp-2013112501 on 2018. 3. 7..
+ */
+
+@RestController
+public class NanaController {
+
+    @GetMapping("/hello")
+    public String hello(){return "Love Nana ♥︎ " ;}
+
+}
